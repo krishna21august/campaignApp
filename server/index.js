34fdbe197f9f27eacc2e04e40301a1b8e602ecde
safe <krishna.s@lucideustech.com>
2019-05-06 09:38:20 +0530

    integrate passport with the application

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
